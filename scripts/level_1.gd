93ee1bbe29236e9766c091bd657c3f2678c96174
Olivier FAURE <couteaubleu@gmail.com>
2024-01-19 15:33:10 +0100

    Add darkness shader, torches and respawn

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -5,13 +5,19 @@ var node_player
 var node_camera
 var start_pos
 var node_timer
+var node_halo
+var level_started = false
+@export var offset = Vector2(56,43)
+@export var zoom_speed = float(0.003)
+#@onready var screensize = get_viewport().size.x 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	node_player = get_node("player")
 	node_camera = get_node("player/Camera2D")
 	node_timer = get_node("Timer")
+	node_halo = get_node("BackBufferCopy/halo")
 	start_pos = node_player.position
-	node_light = get_node("player/PointLight2D")
+
 
 
 
@@ -19,25 +25,34 @@ func _ready():
 func _process(_delta):
 #	/* if player is moving, camera zoom = 0.6 with smooth transition
 #	if player not moving, camera_zoom = 0.4 with smooth transition
-	if node_player.velocity :
-		if node_camera.zoom.x < 0.6 :
-			node_camera.zoom += Vector2(.01,.01)
-	else :
-		if node_camera.zoom.x > 0.4 :
-			node_camera.zoom -= Vector2(0.01,.01)
+	_zoom()
 	var light_size
+	if node_player.velocity.x and !level_started:
+		node_timer.start()
+		level_started = true
+	
 	if !node_timer.is_stopped() :
 		light_size = node_timer.time_left/1.2
 	else : 
 		light_size = node_timer.wait_time/1.2
-	node_light.scale = Vector2(1,1) * light_size
+	node_halo.scale = Vector2(1,1) * light_size
+	node_halo.position = node_player.position + offset
 
+func _zoom():
+	if node_player.velocity and !node_timer.is_stopped() :
+		if node_camera.zoom.x < 0.6 :
+			node_camera.zoom += Vector2(zoom_speed,zoom_speed)
+	else :
+		if node_camera.zoom.x > 0.4 :
+			node_camera.zoom -= Vector2(zoom_speed,zoom_speed)
 
 func _on_timer_timeout():
 	node_player.position = start_pos
+	level_started = false
 
 func _on_camp_fire_body_entered(_body):
 	print("area_entered")
+	get_node("BackBufferCopy/fire").scale = Vector2(1,1) * node_timer.wait_time/1.2
 	node_timer.start()
 	node_timer.stop()
 
